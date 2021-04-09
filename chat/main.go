ec87139ee3da7ddd81f7647e7cb4390acdea6e17
Yuta Segawa <yuta.segawa@quad.co.jp>
2021-04-09 18:15:13 +0900

    feat: ポートを指定できるよう変更

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -18,13 +19,17 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.fileName)))
 	})
-	if err := t.templ.Execute(w, nil); err != nil {
+	if err := t.templ.Execute(w, r); err != nil {
 		log.Fatal("ServeHTTP:", err)
 	}
 }
 
 func main() {
 	log.Print("start chat server..")
+
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+
 	r := newRoom()
 
 	// ルート
@@ -35,7 +40,8 @@ func main() {
 	go r.run()
 
 	// webサーバを起動
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Printf("Webサーバを開始します（ポート: %s）", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
