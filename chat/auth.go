a693458588ac7bc129b48af8ffb93b12b65d1867
Yuta Segawa <yuta.segawa@quad.co.jp>
2021-04-15 11:59:11 +0900

    feat: add loginHandler

--- chat/auth.go
+++ chat/auth.go
@@ -1,12 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
 
 type authHandler struct {
 	next http.Handler
 }
 
-func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
 		// 未認証
 		w.Header().Set("Location", "/login")
@@ -22,4 +27,24 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
 
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
+}
+
+// loginHandler はサードパーティへのログイン処理を受け持ちます
+// パスの形式： /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segments := strings.Split(r.URL.Path, "/")
+	if len(segments) < 3 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "URLが不正です")
+	}
+
+	action := segments[2]
+	provider := segments[3]
+	switch action {
+	case "login":
+		log.Println("TODO: ログイン処理", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション%sには非対応です", action)
+	}
 }
